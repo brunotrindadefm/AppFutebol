7f1302fe5ed48ee6c61a5b895f37b42f330d9393
GustavoCosta10 <gustavocosta803.54@gmail.com>
2025-11-26 20:46:44 -0300

    Terminei a tela de time com os cards

--- src/app/time/tela-time-componente/tela-time-componente.ts
+++ src/app/time/tela-time-componente/tela-time-componente.ts
@@ -1,11 +1,58 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tela-time-componente',
-  imports: [],
+  selector: 'app-tela-time',
+  standalone: true,           
+  imports: [NgFor],                
   templateUrl: './tela-time-componente.html',
-  styleUrl: './tela-time-componente.scss',
+  styleUrls: ['./tela-time-componente.scss']
 })
+
 export class TelaTimeComponente {
 
+  times = [
+    {
+      nome: 'Flamengo',
+      cidade: 'Rio de Janeiro - RJ',
+      estadio: 'Maracanã',
+      fundacao: 1895,
+      pontos: 28
+    },
+    {
+      nome: 'Palmeiras',
+      cidade: 'São Paulo - SP',
+      estadio: 'Allianz Parque',
+      fundacao: 1914,
+      pontos: 30
+    },
+    {
+      nome: 'Corinthians',
+      cidade: 'São Paulo - SP',
+      estadio: 'Neo Química Arena',
+      fundacao: 1910,
+      pontos: 25
+    },
+    {
+      nome: 'São Paulo',
+      cidade: 'São Paulo - SP',
+      estadio: 'Morumbi',
+      fundacao: 1930,
+      pontos: 27
+    },
+    {
+      nome: 'Internacional',
+      cidade: 'Porto Alegre - RS',
+      estadio: 'Beira-Rio',
+      fundacao: 1909,
+      pontos: 26
+    },
+    {
+      nome: 'Grêmio',
+      cidade: 'Porto Alegre - RS',
+      estadio: 'Arena do Grêmio',
+      fundacao: 1903,
+      pontos: 24
+    }
+  ];
 }
